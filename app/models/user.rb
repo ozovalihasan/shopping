60d22125b886fe4aaac9b19fa9439fd828e1f1ac
ozovalihasan <ozovalihasan@gmail.com>
2022-09-07 20:43:11 +0300

    Add devise gem with User class

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum type: {
+    'Customer' => 0,
+    'Seller'=> 1,
+    'Admin' => 2
+  }, _prefix: true
+
+  
+end

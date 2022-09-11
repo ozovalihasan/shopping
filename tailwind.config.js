919b421267a39427da135df9b5aa4f06098eaba8
ozovalihasan <ozovalihasan@gmail.com>
2022-09-11 12:56:57 +0300

    Update the config file of Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
+    './app/components/**/*.html.erb',
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      first: colors.emerald,
+      second: colors.indigo,
+      third: colors.yellow,
+      fourth: colors.slate,
+    },
+  },
 }

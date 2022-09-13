df63809f1207b3f5424cd02129371aae20c9d8ca
ozovalihasan <ozovalihasan@gmail.com>
2022-09-13 14:45:36 +0300

    Update associations of models inheriting from User

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,2 +1,6 @@
 class Seller < User
+  has_many :products, foreign_key: "seller_id"
+  has_many :reviews, through: :products
+  has_many :customers, through: :products
+  has_many :answers, foreign_key: "seller_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,4 @@ class User < ApplicationRecord
     'Seller'=> 1,
     'Admin' => 2
   }, _prefix: true
-  
-  has_many :products, foreign_key: "seller_id"
-  has_many :reviews, through: :products
-  has_many :customers, through: :products
-  has_many :reviews, foreign_key: "customer_id"
-  has_many :answers, foreign_key: "seller_id"
-  has_many :questions, foreign_key: "customer_id"
-  has_many :orders, foreign_key: "customer_id"
 end

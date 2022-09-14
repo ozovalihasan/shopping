f1375dad9434ac14304e67249f28b4bd63061e87
ozovalihasan <ozovalihasan@gmail.com>
2022-09-14 16:28:56 +0300

    Add buttons to change the quantity of an order item

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -86,6 +86,6 @@ class OrderItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_item_params
-      params.require(:order_item).permit(:product_id)
+      params.require(:order_item).permit(:quantity, :product_id)
     end
 end

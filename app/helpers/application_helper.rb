270be42d577e862f9c7689793f27ac35cdaf3c43
ozovalihasan <ozovalihasan@gmail.com>
2022-09-20 15:58:53 +0300

    Add a mechanism used to update order's customer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,19 @@ module ApplicationHelper
     order = Order.find_by id: session[:order_id]
     
     if order
-      if order.customer && (order.customer != current_customer) || order.status_completed?
+      if (order.customer && (order.customer != current_customer)) || order.status_completed?
         order = Order.create(status: 0, total_price: 0.0)  
         session[:order_id] = order.id
       end
+      if order.customer.nil? && customer_signed_in?
+        order.update_attribute :customer, current_customer
+      end
+
     else
       order = Order.create(status: 0, total_price: 0.0)
       session[:order_id] = order.id
     end
+    
     order
   end
 

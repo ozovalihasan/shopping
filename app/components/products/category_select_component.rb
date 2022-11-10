f277b931653a16acaa7e18aa6832335e58f1453b
ozovalihasan <ozovalihasan@gmail.com>
2022-11-10 13:34:36 +0300

    Remove unused components

--- app/components/products/category_select_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Products::CategorySelectComponent < ViewComponent::Base
-  def initialize(categories:, category: )
-    @categories = categories
-    @category = category
-  end
-end

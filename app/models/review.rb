c1f897953aa9dfc1280028c7c4f30e11ee766fdd
ozovalihasan <ozovalihasan@gmail.com>
2022-11-11 21:53:27 +0300

    Update Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :product
   belongs_to :customer
+
+  after_commit :update_product
+
+  def update_product
+    product.update(review_count: product.reviews.count, total_review_score: product.reviews.sum(:rate))
+  end
+  
 end

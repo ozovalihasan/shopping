2d683ac364eeeb84d39b8a5596fc55cb43ed78d7
ozovalihasan <ozovalihasan@gmail.com>
2022-11-17 15:25:10 +0300

    Update the test of the Categories::ShowComponent

--- spec/components/categories/show_component_spec.rb
+++ spec/components/categories/show_component_spec.rb
@@ -3,13 +3,45 @@
 require "rails_helper"
 
 RSpec.describe Categories::ShowComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 
+  it "renders something useful" do
+    
+    # allow(Products::IndexComponent)
+    #   .to receive(:new)
+    #   .and_return("Hi")
+
+    # allow_any_instance_of(Products::IndexComponent)
+    #   .to receive(:render)
+
+    FactoryBot.create(:main_category)    
+    FactoryBot.create(:category)    
+
+    render_inline( Categories::ShowComponent.new(category: Category.first)) do |c|
+      c.with_products do 
+        "Mock products"
+      end
+    end
+
+    expect(rendered_component).to have_text "Mock products"
+    
+    # expect(
+    #   p render_inline(Categories::ShowComponent.new(products: products, category: Category.first, last_page: 2)).css("div").to_html
+    # ).to include(
+    #   "Hello, components!"
+    # )
+  end
+  
   # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
+
+  #   render_inline(Categories::ShowComponent.new(products: products, category: Category.first, last_page: 2))
+
+  #   expect(rendered_component).to have_text "name1"
+    
+  #   # expect(
+  #   #   p render_inline(Categories::ShowComponent.new(products: products, category: Category.first, last_page: 2)).css("div").to_html
+  #   # ).to include(
+  #   #   "Hello, components!"
+  #   # )
   # end
 end

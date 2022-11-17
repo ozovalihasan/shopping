acc9b7553c21b85f6ab03b18317870616067425c
ozovalihasan <ozovalihasan@gmail.com>
2022-11-17 11:30:44 +0300

    Refactor Carts::NavbarComponent

--- app/components/carts/navbar_component.rb
+++ app/components/carts/navbar_component.rb
@@ -4,4 +4,12 @@ class Carts::NavbarComponent < ViewComponent::Base
   def initialize(cart:)
     @cart = cart
   end
+
+  def cart_empty?
+    @cart.order_items.empty?
+  end
+
+  def cart_total_quantity
+    @cart.order_items.sum(&:quantity)
+  end
 end

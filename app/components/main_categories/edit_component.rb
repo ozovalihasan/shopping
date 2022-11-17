ec0f11f59326109a928661b73b66d89c239db7f6
ozovalihasan <ozovalihasan@gmail.com>
2022-11-17 16:06:04 +0300

    Fix components of MainCategories

--- app/components/main_categories/edit_component.rb
+++ app/components/main_categories/edit_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MainCategories::EditComponent < ViewComponent::Base
-  def initialize(main_category:, :)
+  def initialize(main_category:)
     @main_category = main_category
     
   end

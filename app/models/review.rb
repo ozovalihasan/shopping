32be96679ff36febab2e5dd8ad6dc4a35b8d3c62
ozovalihasan <ozovalihasan@gmail.com>
2022-11-18 11:27:26 +0300

    Fix an error raising when a product is destroyed

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
   after_commit :update_product
 
   def update_product
+    return if product.destroyed?
+    
     product.update(
       review_count: product.reviews.count, 
       average_rate: ( product.reviews.average(:rate) || 0).floor(2)

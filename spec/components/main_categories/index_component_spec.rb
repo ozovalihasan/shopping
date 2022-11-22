0630fba9f35680d2f381f8e2e9c90eb33ef43781
ozovalihasan <ozovalihasan@gmail.com>
2022-11-22 15:30:00 +0300

    Update the test of MainCategories::IndexComponent

--- spec/components/main_categories/index_component_spec.rb
+++ spec/components/main_categories/index_component_spec.rb
@@ -3,13 +3,25 @@
 require "rails_helper"
 
 RSpec.describe MainCategories::IndexComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) {
+    FactoryBot.reload
+    FactoryBot.create(:mock_main_category)
+    FactoryBot.create_list(:mock_category, 1)
+    FactoryBot.create(:mock_seller)
+    FactoryBot.create(:mock_product)
+  }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders something useful", :snapshot do
+    allow(Categories::CategoryComponent).to receive(:with_collection) { MockComponent.new( Categories::CategoryComponent ) }
+    allow(Products::ProductComponent).to receive(:with_collection) { MockComponent.new( Products::ProductComponent ) }
+    # allow_any_instance_of(ViewComponent::Base).to receive(:render).and_return("one of unimportant mock components")
+
+    render_inline(described_class.new(products: Product.all, main_categories: MainCategory.all))
+    
+    expect(page).to have_css("turbo-frame#main-parts")
+    expect(page).to have_text( Categories::CategoryComponent.name )
+    expect(page).to have_text( Products::ProductComponent.name )
+      
+  end
+
 end

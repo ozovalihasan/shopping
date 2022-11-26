9b4395daaad7ba4b03cab76ee5dfaaab62a7b1d2
ozovalihasan <ozovalihasan@gmail.com>
2022-11-26 20:30:25 +0300

    Add a test for Categories::IndexComponent

--- spec/components/categories/index_component_spec.rb
+++ spec/components/categories/index_component_spec.rb
@@ -3,13 +3,19 @@
 require "rails_helper"
 
 RSpec.describe Categories::IndexComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders something useful" do
+    
+    FactoryBot.create(:main_category)    
+    FactoryBot.create(:category)    
+
+    render_inline( Categories::IndexComponent.new(category: Category.first)) do |c|
+      c.with_products do 
+        "Mock products"
+      end
+    end
+
+    expect(rendered_content).to have_text "Mock products"
+    
+  end
 end

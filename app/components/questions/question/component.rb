af193eb6e50c3ac37ebc29f53bdc7971362e1fcc
ozovalihasan <ozovalihasan@gmail.com>
2023-06-09 14:04:00 +0300

    Add a collection parameter to Questions::Question::Component

--- app/components/questions/question/component.rb
+++ app/components/questions/question/component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Questions::Question::Component < Application::Component
+  with_collection_parameter :question
+  
   def initialize(question:)
     @question = question
   end

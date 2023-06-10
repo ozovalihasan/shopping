887a13cbbef235e5ba23fe42122a2bf9cbdfccb1
ozovalihasan <ozovalihasan@gmail.com>
2023-06-10 22:33:13 +0300

    Update models for campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,8 +1,9 @@
 class Campaign < ApplicationRecord
   has_many :campaign_items
+  has_many :products, through: :campaign_items
   has_many :order_items
 
   scope :active_at, -> ( time) { where("? BETWEEN start_time AND end_time", time) }
   scope :active_now, -> { active_at( Time.now ) }
-  scope :most_profitable_now, -> { active_now.order(discount: :asc).limit(1) }
+  scope :order_by_discount, -> { order(discount: :desc) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :campaign_items
   has_many :campaigns, through: :campaign_items
+  has_many :active_campaigns_now, -> { active_now }, through: :campaign_items, source: :campaign
   
   has_many_attached :images
 
@@ -18,4 +19,11 @@ class Product < ApplicationRecord
       return where('name ILIKE ?', "%#{search_term}%") 
     end
   end
+
+  scope :with_discount, -> {
+    select("products.*, MAX(campaigns.discount) as discount")
+    .left_outer_joins(:active_campaigns_now)
+    .group( Product.arel_table[:id] )
+  }
+  
 end

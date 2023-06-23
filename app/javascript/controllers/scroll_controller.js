5e5899393a5918f2016c1a1a18fcde324e9b472e
ozovalihasan <ozovalihasan@gmail.com>
2023-06-23 21:17:20 +0300

    Update the scroll behaviour

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,11 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
+import { Turbo } from "@hotwired/turbo-rails";
 
 export default class extends Controller {
-  static targets = [ "navbar", "tab"]
+  static targets = [ "navbar"]
 
-  scrollNow(){
-    this.tabTarget.style.setProperty("scroll-margin-top", `${this.navbarTarget.offsetHeight}px`);  
-    this.tabTarget.scrollIntoView({behavior: "smooth", block: "start"});
+  connect() {
+    this._setScrollMarginTop();
+    document.addEventListener("turbo:visit", this.scrollNow)
   }
-    
+
+  _setScrollMarginTop() {
+    this.element.style.setProperty("scroll-margin-top", `${this.navbarTarget.offsetHeight + 50}px`);  
+  }
+
+  scrollNow(e){
+    if (!(e.type === "turbo:visit") || Turbo.navigator.currentVisit.action === "advance"){
+      e.target.scrollIntoView({behavior: "smooth", block: "start"});
+    }
+  }
 }
